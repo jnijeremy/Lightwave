e9f4154c4a868e43c5899c5c047576f99948b0c6
Jenny Song <jenny.jieun.song@gmail.com>
2016-08-06 23:56:34 -0700

    request

--- test.js
+++ test.js
@@ -1,7 +1,3 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
 var request = require('request');
 	options = {
 		url: 'https://lightwave.atlassian.net/rest/api/2/search?jql=assignee=test',
